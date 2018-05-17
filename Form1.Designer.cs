26d5f24a1ba4204c6fd77756c5cc395d9412fb45
Svildr <svildr@gmail.com>
2018-05-17 18:24:58 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace GifCreator {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnlImages = new System.Windows.Forms.Panel();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.tbFrameDelay = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cB_Forward = new System.Windows.Forms.CheckBox();
+            this.btnFastSave = new System.Windows.Forms.Button();
+            this.cBremoveStatic = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picMain
+            // 
+            this.picMain.Location = new System.Drawing.Point(0, 0);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(1062, 569);
+            this.picMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMain.TabIndex = 1;
+            this.picMain.TabStop = false;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlMain.Controls.Add(this.picMain);
+            this.pnlMain.Location = new System.Drawing.Point(0, 1);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(1065, 572);
+            this.pnlMain.TabIndex = 3;
+            // 
+            // pnlImages
+            // 
+            this.pnlImages.Location = new System.Drawing.Point(0, 593);
+            this.pnlImages.Name = "pnlImages";
+            this.pnlImages.Size = new System.Drawing.Size(843, 103);
+            this.pnlImages.TabIndex = 4;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(974, 610);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 26);
+            this.btnPreview.TabIndex = 3;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(985, 639);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(59, 26);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "gif";
+            // 
+            // tbFrameDelay
+            // 
+            this.tbFrameDelay.Location = new System.Drawing.Point(883, 612);
+            this.tbFrameDelay.Name = "tbFrameDelay";
+            this.tbFrameDelay.Size = new System.Drawing.Size(80, 22);
+            this.tbFrameDelay.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(880, 592);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Frame Delay";
+            // 
+            // cB_Forward
+            // 
+            this.cB_Forward.AutoSize = true;
+            this.cB_Forward.Location = new System.Drawing.Point(974, 591);
+            this.cB_Forward.Name = "cB_Forward";
+            this.cB_Forward.Size = new System.Drawing.Size(81, 21);
+            this.cB_Forward.TabIndex = 8;
+            this.cB_Forward.Text = "Forward";
+            this.cB_Forward.UseVisualStyleBackColor = true;
+            // 
+            // btnFastSave
+            // 
+            this.btnFastSave.Location = new System.Drawing.Point(877, 640);
+            this.btnFastSave.Name = "btnFastSave";
+            this.btnFastSave.Size = new System.Drawing.Size(80, 26);
+            this.btnFastSave.TabIndex = 9;
+            this.btnFastSave.Text = "Fast Save";
+            this.btnFastSave.UseVisualStyleBackColor = true;
+            this.btnFastSave.Click += new System.EventHandler(this.btnFastSave_Click);
+            // 
+            // cBremoveStatic
+            // 
+            this.cBremoveStatic.AutoSize = true;
+            this.cBremoveStatic.Location = new System.Drawing.Point(877, 672);
+            this.cBremoveStatic.Name = "cBremoveStatic";
+            this.cBremoveStatic.Size = new System.Drawing.Size(121, 21);
+            this.cBremoveStatic.TabIndex = 10;
+            this.cBremoveStatic.Text = "Remove Static";
+            this.cBremoveStatic.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 702);
+            this.Controls.Add(this.btnFastSave);
+            this.Controls.Add(this.cBremoveStatic);
+            this.Controls.Add(this.cB_Forward);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFrameDelay);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.pnlImages);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            this.pnlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Panel pnlImages;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TextBox tbFrameDelay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cB_Forward;
+        private System.Windows.Forms.Button btnFastSave;
+        private System.Windows.Forms.CheckBox cBremoveStatic;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GifCreator {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private List<Mage> listMage = new List<Mage>();
+        private int indexer = 0;
+        private string ArchiveName = "";
+        private string fastDirectory = "";
+
+        private Image loadImage(string path) {
+            string ext = path.Split('.').Last().ToLower();
+            ImageFormat imgFormat = ImageFormat.Jpeg;
+
+
+            var f = new FileInfo(path);
+            //ArchiveName = f.Directory.Name;
+            ArchiveName = f.Name.Replace(f.Extension, "");
+            fastDirectory = f.DirectoryName;
+            //ArchiveName = ArchiveName.Remove(0, 5);
+
+            switch (ext) {
+                case "gif":
+                    imgFormat = ImageFormat.Gif;
+                    break;
+                case "bmp":
+                    imgFormat = ImageFormat.Bmp;
+                    break;
+                case "ico":
+                    imgFormat = ImageFormat.Icon;
+                    break;
+                case "png":
+                    imgFormat = ImageFormat.Png;
+                    break;
+                case "tiff":
+                    imgFormat = ImageFormat.Tiff;
+                    break;
+                default:
+                    imgFormat = ImageFormat.Jpeg;
+                    break;
+            }
+
+            MemoryStream mS = new MemoryStream();
+            using (Image i = Image.FromFile(path)) {
+                i.Save(mS, imgFormat);
+            }
+
+            return Image.FromStream(mS);
+        }
+        private Image ResizeImage(Image img, int width, int height) {
+            Rectangle destRect = new Rectangle(0, 0, width, height);
+            Bitmap destBitmap = new Bitmap(width, height);
+
+            destBitmap.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+
+            using (Graphics graph = Graphics.FromImage(destBitmap)) {
+                graph.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+                graph.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighSpeed;
+                graph.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Low;
+                graph.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+                graph.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighSpeed;
+
+                using (ImageAttributes wrap = new ImageAttributes()) {
+                    wrap.SetWrapMode(System.Drawing.Drawing2D.WrapMode.TileFlipXY);
+                    graph.DrawImage(img, destRect, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, wrap);
+                }
+            }
+
+            return destBitmap;
+        }
+
+        private MemoryStream GenerateGif() {
+            int frameDelay = 500;
+            if (!string.IsNullOrEmpty(tbFrameDelay.Text)) {
+                frameDelay = Convert.ToInt32(tbFrameDelay.Text);
+            }
+
+            MemoryStream mS = new MemoryStream();
+            GifWriter gifWriter = new GifWriter(mS, frameDelay);
+            for (int i = 0; i < listMage.Count(); i++) {
+                gifWriter.WriteFrame(listMage[i].image);
+            }
+
+            if (cB_Forward.Checked) {
+                for (int i = listMage.Count() - 2; i > 0; i--) {
+                    gifWriter.WriteFrame(listMage[i].image);
+                }
+            }
+
+            return mS;
+        }
+        public class Mage {
+            public int id { get; set; }
+            public Image image { get; set; }
+            public string Path { get; set; }
+
+            public PictureBox pictureBox { get; set; }
+            public Button btnBackward { get; set; }
+            public Button btnForward { get; set; }
+            public Button btnRemove { get; set; }
+        }
+
+        #region Buttons and Icons
+        private void Form1_DragDrop(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e) {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in files) {
+                listMage.Add(createMage(file));
+            }
+        }
+
+        private void pnlDrop_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void pnlDrop_DragDrop(object sender, DragEventArgs e) {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in files) {
+                listMage.Add(createMage(file));
+            }
+        }
+
+        private void btnBackward_Click(object sender, EventArgs e) {
+            int posX = listMage.IndexOf(listMage.Where(p => p.btnBackward.Name == ((Button)sender).Name).FirstOrDefault());
+            int id = listMage[posX].id;
+
+            if (id == 0) {
+                return; // Already at the start
+            }
+
+            int posY = listMage.IndexOf(listMage.Where(p => p.id == id - 1).FirstOrDefault());
+
+            listMage[posX].id = id - 1;
+            listMage[posY].id = id;
+
+            reorganizeThumbs();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e) {
+            int posX = listMage.IndexOf(listMage.Where(p => p.btnForward.Name == ((Button)sender).Name).FirstOrDefault());
+            int id = listMage[posX].id;
+
+            if (id == listMage.Count() - 1) {
+                return; // Already at the end
+            }
+
+            int posY = listMage.IndexOf(listMage.Where(p => p.id == id + 1).FirstOrDefault());
+
+            listMage[posX].id = id + 1;
+            listMage[posY].id = id;
+
+            reorganizeThumbs();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e) {
+            int posX = listMage.IndexOf(listMage.Where(p => p.btnRemove.Name == ((Button)sender).Name).FirstOrDefault());
+
+            listMage[posX].pictureBox.Dispose();
+            listMage[posX].btnBackward.Dispose();
+            listMage[posX].btnForward.Dispose();
+            listMage[posX].btnRemove.Dispose();
+
+            listMage.RemoveAt(posX);
+
+            reorganizeThumbs();
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e) {
+            picMain.Image = Image.FromStream(GenerateGif());
+
+            decimal diffWidth = (decimal)pnlMain.Width / picMain.Image.Width;
+            decimal diffHeight = (decimal)pnlMain.Height / picMain.Image.Height;
+
+            decimal min = diffWidth > diffHeight ? diffHeight : diffWidth;
+
+            if (min < 1) {
+                picMain.Height = (int)(picMain.Image.Height * min);
+                picMain.Width = (int)(picMain.Image.Width * min);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            Image img = Image.FromStream(GenerateGif());
+
+            saveFileDialog.FileName = ArchiveName;
+
+            saveFileDialog.ShowDialog();
+            string savePath = saveFileDialog.FileName;
+
+            img.Save(savePath, ImageFormat.Gif);
+        }
+
+        private void btnFastSave_Click(object sender, EventArgs e) {
+            Image img = Image.FromStream(GenerateGif());
+
+            string fullPath =  fastDirectory + "\\" + ArchiveName + ".gif";
+            img.Save(fullPath, ImageFormat.Gif);
+
+            if (cBremoveStatic.Checked) {
+                    foreach (var mage in listMage) {
+                    File.Delete(mage.Path);
+                }
+            }
+
+            Clear();
+        }
+        #endregion
+
+        #region Create and Manange Thumbs
+        private Mage createMage(string path) {
+            Image img = loadImage(path);
+
+            PictureBox pictureBox = new PictureBox();
+            Button btnBackward = new Button();
+            Button btnForward = new Button();
+            Button btnRemove = new Button();
+
+            pictureBox.Location = new Point(12 + (106 * listMage.Count()), 5);
+            pictureBox.Name = "picImageThumb_" + indexer;
+            pictureBox.Size = new Size(100, 66);
+            pictureBox.Image = ResizeImage(img, 100, 66);
+
+            btnBackward.Location = new Point(12 + (106 * listMage.Count()), 76);
+            btnBackward.Name = "btnBackward_" + indexer;
+            btnBackward.Size = new Size(27, 23);
+            btnBackward.Text = "<";
+            btnBackward.UseVisualStyleBackColor = true;
+            btnBackward.Click += new EventHandler(this.btnBackward_Click);
+
+            btnForward.Location = new Point(38 + (106 * listMage.Count()), 76);
+            btnForward.Name = "btnForward_" + indexer;
+            btnForward.Size = new Size(27, 23);
+            btnForward.Text = ">";
+            btnForward.UseVisualStyleBackColor = true;
+            btnForward.Click += new EventHandler(this.btnForward_Click);
+
+            btnRemove.Location = new Point(77 + (106 * listMage.Count()), 76);
+            btnRemove.Name = "btnRemove_" + indexer;
+            btnRemove.Size = new Size(35, 23);
+            btnRemove.Text = "X";
+            btnRemove.UseVisualStyleBackColor = true;
+            btnRemove.Click += new EventHandler(this.btnRemove_Click);
+
+            pnlImages.Controls.Add(pictureBox);
+            pnlImages.Controls.Add(btnBackward);
+            pnlImages.Controls.Add(btnForward);
+            pnlImages.Controls.Add(btnRemove);
+
+            indexer++;
+
+            return new Mage() {
+                id = listMage.Count(),
+                Path = path,
+                image = img,
+                pictureBox = pictureBox,
+                btnBackward = btnBackward,
+                btnForward = btnForward,
+                btnRemove = btnRemove
+            };
+        }
+
+        private void reorganizeThumbs() {
+            listMage = listMage.OrderBy(p => p.id).ToList();
+
+            for (int i = 0; i < listMage.Count(); i++) {
+                listMage[i].id = i;
+
+                listMage[i].pictureBox.Location = new Point(12 + (106 * i), 5);
+                listMage[i].btnBackward.Location = new Point(12 + (106 * i), 76);
+                listMage[i].btnForward.Location = new Point(38 + (106 * i), 76);
+                listMage[i].btnRemove.Location = new Point(77 + (106 * i), 76);
+            }
+        }
+
+        private void Clear() {
+            while (listMage.Count() > 0) {
+                listMage[0].pictureBox.Dispose();
+                listMage[0].btnBackward.Dispose();
+                listMage[0].btnForward.Dispose();
+                listMage[0].btnRemove.Dispose();
+
+                listMage.RemoveAt(0);
+            }
+
+            //reorganizeThumbs();
+        }
+        #endregion
+    }
+}
